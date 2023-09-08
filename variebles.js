4785f5ad148fdb015db051be68a448cef6f9a50e
lawitadelimon <lau72abrvjua@gmail.com>
2023-09-08 14:41:33 -0600

    01 comenzando JS

---
+++ variebles.js
@@ -0,0 +1,11 @@
+// JS no es tipado por lo que no se especifica tipo de dato
+//para delaracion de variables 
+//utilizamos const y let
+//const para variables de solo lectura 
+//let para variables
+
+const pi = 3.1416;
+pi = "tres";
+
+const g
+g = 1.89 
